e631736a4180ea181b2b86ea7c3ce5c10ea14047
Ashish K. Shrestha <ashish.ashishshrestha@gmail.com>
2025-08-01 04:20:33 +0545

    chnages

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
-// require("dotenv").config();
-// const mongoose = require("mongoose");
-// const app = require("./app"); // Import the configured Express app
+require("dotenv").config();
+const mongoose = require("mongoose");
+const app = require("./app"); // Import the configured Express app
 
-// // Connect to MongoDB
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => console.log("✅ Connected to MongoDB"))
-//   .catch((err) => console.error("❌ MongoDB connection error:", err));
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ Connected to MongoDB"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-// // Start the server
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server running on http://localhost:${PORT}`);
-// });
+// Start the server
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
