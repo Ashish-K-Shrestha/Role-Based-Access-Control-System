318ab42d1e94ef095007e2e54ba0c0eee0beab63
Ashish K. Shrestha <ashish.ashishshrestha@gmail.com>
2025-08-01 04:27:01 +0545

    updates

--- server/views/google.js
+++ server/views/google.js
@@ -12,12 +12,12 @@ const User = require("../schema/user");
 //creates a strategy to authenticate google accounts
 passport.use(
   new GoogleStrategy(
-    // {
-    //   clientID: process.env.GOOGLE_CLIENT_ID,
-    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    //   callbackURL:
-    //     process.env.GOOGLE_CALLBACK_URL + "/api/auth/google/redirect",
-    // },
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL:
+        process.env.GOOGLE_CALLBACK_URL + "/api/auth/google/redirect",
+    },
     function (accessToken, refreshToken, profile, cb) {
       //find the user if DB if not present then save the usernameField
       User.findOrCreate(
